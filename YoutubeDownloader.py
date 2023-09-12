2c8c48af049142aacf72d4b94396e0e4002dc48c
Starnuto di topo <starnutoditopo@gmail.com>
2023-09-12 15:25:57 +0200

    update script

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,65 +1,44 @@
 #!/usr/local/bin/python
+
 from pytube import YouTube
-import os
+import sys, getopt
 
 def Download(link, output_path):
     try:
         youtube_object = YouTube(link)
         youtube_object = youtube_object.streams.filter(only_audio=True).first()
-        #destination = os.path.join(output_path, f'{youtube_object.title}.mp3')
-        #youtube_object.download(output_path=destination)
         youtube_object.download(output_path=output_path)
     except Exception:
-        print("An error has occurred")
+        print("   An error has occurred")
         return
     
-    print("Download is completed successfully")
+    print("   Download is completed successfully")
 
-def main():
+def main(argv):
+    inputfile = ''
+    output_directory = ''
+    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","odir="])
+    for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -i <inputfile> -o <output_directory>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--odir"):
+         output_directory = arg
+    print ('Input file is: ', inputfile)
+    print ('Output directory is: ', output_directory)
+
     output_path = './output'
-    links = [
-        # 'https://youtu.be/lWA2pjMjpBs',
-        # 'https://youtu.be/kJQP7kiw5Fk',
-        # 'https://youtu.be/HCjNJDNzw8Y',
-        # 'https://youtu.be/DeumyOzKqgI',
-        # 'https://youtu.be/Bg3pFjbCag0',
-        # 'https://youtu.be/rRzJq9GvUjY',
-        # 'https://youtu.be/9jK-NcRmVcw',
-        # 'https://youtu.be/eErgDjAOyz8',
-        # 'https://youtu.be/QkF3oxziUI4',
-        # 'https://youtu.be/uSiHqxgE2d0',
-        # 'https://youtu.be/6xULwPM--KY',
-        # 'https://youtu.be/jx8GhXm-HcA',
-        # 'https://youtu.be/ccqcpOLaMk8',
-        # 'https://youtu.be/2tkEi1aSOYg',
-        # 'https://youtu.be/kyxEpZfh4iA',
-        # 'https://youtu.be/wKvJHFy5kHo',
-        # 'https://youtu.be/09vV1WSJzVs',
-        # 'https://youtu.be/RlxAhvi-j_U',
-        # 'https://youtu.be/PZMJAkSLa0o',
-        # 'https://youtu.be/faL2qm-F1jM',
-        # 'https://youtu.be/eMnxjdGTK4w',
-        # 'https://youtu.be/k1tyVlKjJZI',
-        # 'https://youtu.be/68ugkg9RePc',
-        # 'https://youtu.be/fPO76Jlnz6c',
-        # 'https://youtu.be/1V_xRb0x9aw',
-        # 'https://youtu.be/6qXLem0_mRc',
-        # 'https://youtu.be/k9qUvf8j5Dc',
-        # 'https://youtu.be/0J2QdDbelmY',
-        # 'https://youtu.be/2wUvlTUi8kQ',
-        # 'https://youtu.be/WZ32gSLNHfA',
-        # 'https://youtu.be/4E-1rkj60Z0',
-        # 'https://youtu.be/jcmdzOcyb0w',
-        # 'https://youtu.be/13U43P_dsQ4',
-        # 'https://youtu.be/lN5wpoNJDUk',
-        # 'https://youtu.be/wd9r0xTGhyo',
-        # 'https://youtu.be/n7C1tOOJpsk',
-        # 'https://youtu.be/6Ejga4kJUts',
-        # 'https://youtu.be/8Dd-wDxT07Y',
-        # 'https://youtu.be/IVqnx1LAh7s'
-    ]
-    for link in links:
-        Download(link, output_path)
+    
+    file = open(inputfile, 'r')
+    lines = file.readlines()
+    for line in lines:
+        link = line.strip()
+        if len(link) != 0:
+            print(f'Downloading {link}...')
+            Download(link, output_path)
+            print(f'   ... done.')
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
